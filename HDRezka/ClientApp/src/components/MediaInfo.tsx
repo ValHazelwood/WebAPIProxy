5c83aed23582a6b2c6c5369a7fa698e816bbc0dc
User <master.leva@gmail.com>
2020-04-19 14:53:37 +0300

    Added videojs player wrapper

--- HDRezka/ClientApp/src/components/MediaInfo.tsx
+++ HDRezka/ClientApp/src/components/MediaInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MediaData } from "../store/types";
 import Header from "./Header";
+import VideoPlayer from "./Video";
 
 interface MediaTypes {
     data: MediaData | undefined
@@ -10,15 +11,33 @@ const MediaInfo = ({ data }: MediaTypes) => {
 
     console.log(data);
     if (data && data.searchResult) {
-        return (<React.Fragment><Header title={data.searchResult.name} />
-            <div className="mediaInfo">
-                <p>{data.searchResult.name} {data.searchResult.text} rating: {data.searchResult.rating}</p>
-                <video controls> <source src={data.media.translations[0].cdnStreams[3].urL2} type="video/mp4" /></video>
-            </div>
-        </React.Fragment>);
+
+        if (data.media.type === 0) {
+
+            const videoJsOptions = {
+                sources: [
+                    {
+                        src: data.media.translations[0].cdnStreams[3].urL2,
+                        type: "video/mp4"
+                    }
+                ]
+            };
+
+            return (<React.Fragment><Header title={data.searchResult.name} />
+                <div className="mediaInfo">
+                    <p>{data.searchResult.name} {data.searchResult.text} rating: {data.searchResult.rating}</p>
+                    {/* <video controls> <source src={data.media.translations[0].cdnStreams[3].urL2} type="video/mp4" /></video> */}
+                    <VideoPlayer options={videoJsOptions} />
+                </div>
+            </React.Fragment>);
+        } else {
+            return (<React.Fragment><Header title="Series information" /></React.Fragment>);
+        }
+
     }
 
     return (<React.Fragment><Header title="No media information" /></React.Fragment>);
+
 };
 
 export default MediaInfo;
