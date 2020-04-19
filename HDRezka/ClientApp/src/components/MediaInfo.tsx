0cade5c65844c4e10906450f09d1885f5f4a2ed5
User <master.leva@gmail.com>
2020-04-19 19:51:37 +0300

    Updated MediaInfo, added translation selection

--- HDRezka/ClientApp/src/components/MediaInfo.tsx
+++ HDRezka/ClientApp/src/components/MediaInfo.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { MediaData } from "../store/types";
 import Header from "./Header";
-import VideoPlayer from "./Video";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 interface MediaTypes {
     data: MediaData | undefined
@@ -9,27 +10,57 @@ interface MediaTypes {
 
 const MediaInfo = ({ data }: MediaTypes) => {
 
+    const [currentTranslationId, setCurrentTranslationId] = useState<number | undefined>(data?.media?.currentTranslationId);
+
+    const [currentQualityId, setCurrentQualityId] = useState<string>("480p");
+
     console.log(data);
-    if (data && data.searchResult) {
+
+    if (data && data.searchResult && data.media) {
 
         if (data.media.type === 0) {
 
-            const videoJsOptions = {
-                sources: [
-                    {
-                        src: data.media.translations[0].cdnStreams[3].urL2,
-                        type: "video/mp4"
-                    }
-                ]
-            };
+            let translationsList = data.media.translations.map(x => ({ value: x.id.toString(), label: x.name }));
 
-            return (<React.Fragment><Header title={data.searchResult.name} />
-                <div className="mediaInfo">
-                    <p>{data.searchResult.name} {data.searchResult.text} rating: {data.searchResult.rating}</p>
-                    {/* <video controls> <source src={data.media.translations[0].cdnStreams[3].urL2} type="video/mp4" /></video> */}
-                    <VideoPlayer options={videoJsOptions} />
-                </div>
-            </React.Fragment>);
+            let translationDefaultOption = translationsList.find(x => x.value === currentTranslationId?.toString());
+
+            let translation = data.media.translations.find(x => x.id === currentTranslationId);
+
+            if (translation) {
+                let quality = translation.cdnStreams.find(x => x.quality === currentQualityId);
+
+                let qualityList = translation.cdnStreams.map(x => ({ value: x.quality, label: x.quality }));
+
+                let qualityDefaultOption = qualityList.find(x => x.value === currentQualityId);
+
+                if (quality) {
+
+                    let srcUrl = quality.urL2;
+
+                    const onTranslationSelected = (option: any) => {
+
+                        console.log(option);
+
+                        setCurrentTranslationId(parseInt(option.value));
+                    }
+
+                    const onQualitySelected = (option: any) => {
+
+                        console.log(option);
+
+                        setCurrentQualityId(option.value);
+                    }
+
+                    return (<React.Fragment><Header title={data.searchResult.name} />
+                        <div className="mediaInfo">
+                            <p>{data.searchResult.name} {data.searchResult.text} rating: {data.searchResult.rating}</p>
+                            <p>Translation: <Dropdown className="translationSelect" options={translationsList} onChange={onTranslationSelected} value={translationDefaultOption} /></p>
+                            <p>Quality: <Dropdown className="qualitySelect" options={qualityList} onChange={onQualitySelected} value={qualityDefaultOption} /></p>
+                            <video controls src={srcUrl}> <source src={srcUrl} type="video/mp4" /></video>
+                        </div>
+                    </React.Fragment>);
+                }
+            }
         } else {
             return (<React.Fragment><Header title="Series information" /></React.Fragment>);
         }
